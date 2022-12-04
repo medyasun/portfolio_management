1483812f839c68e63abede9b7dc90d89c5d86511
medyasun <msungur@gmail.com>
2022-12-04 17:12:20 +0300

    test2

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if selected=="Portföy Test Et":
         if len(assets)>7:
             st.error("Portföyünde En Fazla 7 Hisse Olabilir!!!")
         else:
-  
+         
                 df=df_port(assets=assets,startd=startd,endd=endd)
                 precent_return,percent_volatility,percent_var,weights=expected_return(df,assets)
                 chart_return=chart_return(df)
